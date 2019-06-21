6c8af1711c307e7247f71f97103a30c6f719c5e5
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-06-21 14:42:04 -0700

    clean branch with unit test

---
+++ Hello_SpeechRecognitionTests/Hello_SpeechRecognitionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Hello_SpeechRecognitionTests.swift
+//  Hello_SpeechRecognitionTests
+//
+//  Created by Cyril Garcia on 6/21/19.
+//  Copyright © 2019 Cyril Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import Hello_SpeechRecognition
+
+class Hello_SpeechRecognitionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+
+    }
+
+    func testPerformanceExample() {
+
+        self.measure {
+
+        }
+    }
+
+}
