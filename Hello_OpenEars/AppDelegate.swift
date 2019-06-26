fb68e15c1253a4612da0a80e272f6c4a5a092979
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-06-26 15:41:04 -0700

    project with OpenEars

---
+++ Hello_OpenEars/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Hello_OpenEars
+//
+//  Created by Cyril Garcia on 6/26/19.
+//  Copyright © 2019 Cyril Garcia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+
+
+}
+
